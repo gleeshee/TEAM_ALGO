9c5298e0e3f4536551789dea7ecbdee03dd9ed01
MarkJohnnn <162429312+MarkJohnnn@users.noreply.github.com>
2024-06-08 18:00:24 +0800

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,39 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
+<header>
+    <nav class="navbar navbar-expand-lg fixed-top">
+        <div class="container-fluid">
+            <a class="navbar-brand me-auto" href="#">BarangayConnect</a>
+            <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                <div class="offcanvas-header">
+                    <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Logo</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+                <div class="offcanvas-body">
+                    <ul class="navbar-nav justify-content-center flex-grow-1 pe-3">
+                        <li class="nav-item">
+                            <a class="nav-link active mx-lg-2" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link mx-lg-2" href="#" data-bs-toggle="modal" data-bs-target="#aboutModal">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link mx-lg-2" href="#" data-bs-toggle="modal" data-bs-target="#serviceModal">Service</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link mx-lg-2" href="#" data-bs-toggle="modal" data-bs-target="#contactModal">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <button class="login_btn" data-bs-toggle="modal" data-bs-target="#loginModal">Login</button>
+            <button class="navbar-toggler pe-0" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
+    </nav>
+</header>
 
 </body>
 </html>
