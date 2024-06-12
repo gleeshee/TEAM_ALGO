21e469a8320ce12352383c7a5be544425583e3c9
Cherieee27 <162429665+Cherieee27@users.noreply.github.com>
2024-06-12 08:37:18 +0800

    Index.php

--- index.php
+++ index.php
@@ -1,11 +1,18 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>BarangayConnect</title>
-         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="BarangayConnect - Connecting communities to build a stronger neighborhood.">
+    <title>BarangayConnect</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
+    <style>
+        .modal-content {
+    box-shadow: 0 8px 16px black, 0 12px 40px white;
+    border-radius: 8px;
+}
+    </style>
 </head>
 <body>
 <header>
@@ -41,6 +48,7 @@
         </div>
     </nav>
 </header>
+
 <main>
     <!-- HERO-->
     <section class="hero-section">
@@ -56,6 +64,7 @@
     </section>
     <!-- End  -->
 </main>
+
 <!-- About -->
 <div class="modal fade" id="aboutModal" tabindex="-1" aria-labelledby="aboutModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered">
@@ -72,6 +81,7 @@
         </div>
     </div>
 </div>
+
 <!-- Service Modal -->
 <div class="modal fade" id="serviceModal" tabindex="-1" aria-labelledby="serviceModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered">
@@ -88,7 +98,8 @@
         </div>
     </div>
 </div>
-    <!-- Contact -->
+
+<!-- Contact -->
 <div class="modal fade" id="contactModal" tabindex="-1" aria-labelledby="contactModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content"style="background-color: black;" >
@@ -104,6 +115,7 @@
         </div>
     </div>
 </div>
+
 <!-- Login -->
 <div class="modal fade" id="loginModal" tabindex="-1" aria-labelledby="loginModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered">
@@ -156,6 +168,7 @@
         </div>
     </div>
 </div>
+
 <!-- Signup -->
 <div class="modal fade" id="signupModal" tabindex="-1" aria-labelledby="signupModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered">
@@ -190,6 +203,5 @@
 </div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-
 </body>
 </html>
