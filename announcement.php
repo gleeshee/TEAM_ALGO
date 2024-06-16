2d8f64aa72ef2cdc53e6507314eff0884b75925c
gleeshee <162429256+gleeshee@users.noreply.github.com>
2024-06-16 12:22:12 +0800

    Update announcement.php

--- announcement.php
+++ announcement.php
@@ -16,3 +16,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $stmt = $pdo->prepare($sql);
     $stmt->bindParam(':title', $title);
     $stmt->bindParam(':details', $details);
+if ($stmt->execute()) {
+        echo '<script>alert("Announcement added successfully."); window.location.href = "adminhomepage.php";</script>';
+    } else {
+        echo '<div class="alert alert-danger">Oops! Something went wrong. Please try again later.</div>';
+    }
+
+    unset($stmt);
+}
+
+$pdo = null;
+?>
