4c24cdc9cd151b2e129a17e2a33fcce9a029e7f1
gleeshee <162429256+gleeshee@users.noreply.github.com>
2024-06-16 11:03:14 +0800

    Update adminhomepage.php

--- adminhomepage.php
+++ adminhomepage.php
@@ -21,3 +21,23 @@ if (!$conn) {
     <title>BarangayConnect</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
+href="style.css">
+
+    <style >
+        .modal-body-profile {
+    margin-top: 200px;
+    position: absolute;
+    flex: 1 1 auto;
+    padding: var(--bs-modal-padding);
+}
+      
+    ::-webkit-scrollbar {
+        width: 10px; 
+    }
+    ::-webkit-scrollbar-track {
+        background-color: cornsilk;
+    }
+    ::-webkit-scrollbar-thumb {
+        background-color: white; 
+        border-radius: 10px; 
+    }
