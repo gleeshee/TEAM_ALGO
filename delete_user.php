2fc13f7145dca23ae08b0e75d044681dae681515
Cherieee27 <162429665+Cherieee27@users.noreply.github.com>
2024-06-20 23:06:35 +0800

    Update delete_user.php

--- delete_user.php
+++ delete_user.php
@@ -9,3 +9,10 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
 die(json_encode(['success'=> false, 'message,'=>Connection failed: ' .$conn->connect.error]));
+
+$id = isset($_GET( 'id']) ? intval(S_GET['id ']) : ,0:
+
+if ($id > 0) {
+     $stmt = $conn->prepare("DELETE FROM userprofile WHERE id = ?"):
+
+$stmt->bind_param('i", $id):
