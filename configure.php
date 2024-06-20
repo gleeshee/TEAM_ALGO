a4a625894be35f0518bf518eca9d976bf6dfad64
Cherieee27 <162429665+Cherieee27@users.noreply.github.com>
2024-06-20 21:33:26 +0800

    Update configure.php

--- configure.php
+++ configure.php
@@ -5,3 +5,8 @@ $pass = " ";
 $charset = 'utf8mb4 ';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+     PDO ::ATTR_ERRMODE         => PDO :: ERRMODE_EXCEPTION,
+     PDO ::ATTR_DEFAULT_FETCH_MODE => PDO ::FETCH _ASSOC,
+     PDO ::ATTR_EMULATE_PREPARES     fasle,
+];
