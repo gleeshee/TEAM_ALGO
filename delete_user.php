0c1a17e266b932fbe072a125ceed197f5393bbad
Cherieee27 <162429665+Cherieee27@users.noreply.github.com>
2024-06-20 22:58:49 +0800

    Update delete_user.php

--- delete_user.php
+++ delete_user.php
@@ -6,3 +6,6 @@ $password = "";
 $dbname = "barangayconnect";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+die(json_encode(['success'=> false, 'message,'=>Connection failed: ' .$conn->connect.error]));
