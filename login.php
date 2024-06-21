148e5a15f672db287eaa25ac737bd650aadd2149
Cherieee27 <162429665+Cherieee27@users.noreply.github.com>
2024-06-21 11:16:53 +0800

    Update login.php

--- login.php
+++ login.php
@@ -5,3 +5,9 @@ include 'config.php':
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 $email = $_POST['email'];
 $password = $_POST['password']:
+
+try {
+
+$stmt= $conn->prepare("SELECT FROM users WHERE email= :email"); $stmt->bindParam(':email, $email);
+$stmt->execute();
+$user = $stmt->fetch(PDO::FETCH_ASSOC);
