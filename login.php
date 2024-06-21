ad892a86128410182caa80cf7609075faa05e536
Cherieee27 <162429665+Cherieee27@users.noreply.github.com>
2024-06-21 11:18:51 +0800

    Update login.php

--- login.php
+++ login.php
@@ -8,6 +8,10 @@ $password = $_POST['password']:
 
 try {
 
-$stmt= $conn->prepare("SELECT FROM users WHERE email= :email"); $stmt->bindParam(':email, $email);
+$stmt= $conn->prepare("SELECT FROM users WHERE email= :email"); $stmt->bindParam(':email', $email);
 $stmt->execute();
 $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+if ($user && password_verify($password, $user['password'])) {
+session_start();
+ $_SESSION['email'] = $email;
