df40e71c6cb9b66348333444df1fac95b297f3f8
Cherieee27 <162429665+Cherieee27@users.noreply.github.com>
2024-06-21 11:09:17 +0800

    Update get_user_data.php

--- get_user_data.php
+++ get_user_data.php
@@ -7,3 +7,7 @@ $user = $_GET['user'];
 $sql = "SELECT * FROM userprofile WHERE name = '$user'"; $result = mysqli_query($conn, $sql);
 
 $userData = array();
+
+if (mysqli_num_rows($result) > 0) {
+
+$userData = mysqli_fetch_assoc($result);
